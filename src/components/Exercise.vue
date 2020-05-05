d502f13e9731a123b112d044d6883917f4e0f7d2
Jailson Dias <jcd2@cin.ufpe.br>
2020-05-05 07:29:19 -0300

    adding home student page

---
+++ src/components/Exercise.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card class="my-1" width="100%" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <!-- <div class="overline mb-4">OVERLINE</div> -->
+        <v-list-item-title class="headline mb-1">Atividade 5</v-list-item-title>
+        <!-- <v-list-item-subtitle
+          >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
+        > -->
+        <p>Questão 1 <v-icon color="primary">mdi-check</v-icon></p>
+        <p>Questão 1 <v-icon color="primary">mdi-close</v-icon></p>
+        <p>Questão 1</p>
+        <p>Questão 1</p>
+      </v-list-item-content>
+
+      <!-- <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar> -->
+    </v-list-item>
+
+    <!-- <v-card-actions>
+      <v-btn text>Button</v-btn>
+      <v-btn text>Button</v-btn>
+    </v-card-actions> -->
+  </v-card>
+</template>
