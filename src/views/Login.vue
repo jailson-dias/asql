0e97952a90b8f59bb7f0a8adf8b06e56d147ab1b
Jailson Dias <jailson.dias@dmcard.com.br>
2020-05-21 20:55:23 -0300

    adjusts on login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,18 @@
 <template>
   <v-app id="inspire">
     <v-content>
-      <v-container class="fill-height background-image-asql" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <v-card class="pa-5 text-center" max-width="450">
-              <img src="@/assets/logo.png" alt="Logo" width="150" />
+      <v-container class="fill-height background-image-asql py-0" fluid>
+        <v-row align="center fill-height background-color-asql" justify="center">
+          <v-col cols="12" sm="9" md="5">
+            <v-card class="py-12 px-12 text-center" max-width="550">
+              <img class="" src="@/assets/logo.png" alt="Logo" width="150" />
+              <h1 class="display-2">Login</h1>
+              <p class="headline font-regular mx-8 mt-4">Digite o email e senha para fazer login</p>
               <v-form ref="form" v-model="valid" lazy-validation>
-                <v-text-field
-                  v-model="email"
-                  label="E-mail"
-                  required
-                ></v-text-field>
+                <v-text-field v-model="email" label="E-mail" required outlined></v-text-field>
 
-                <v-text-field
-                  v-model="password"
-                  label="Senha"
-                  type="password"
-                  required
-                ></v-text-field>
-                <v-row class="ma-0" justify="space-between">
+                <v-text-field class="mt-n3" v-model="password" label="Senha" type="password" required outlined></v-text-field>
+                <v-row class="mx-0 mt-n6" justify="space-between">
                   <a>Sou professor</a>
                   <a>Cadastrar</a>
                 </v-row>
@@ -29,13 +22,13 @@
                   :rules="[v => !!v || 'You must agree to continue!']"
                   label="Do you agree?"
                   required
-                ></v-checkbox> -->
+                ></v-checkbox>-->
 
-                <v-btn to="/" class="mt-6 mb-3" color="primary">Login</v-btn>
+                <v-btn to="/" class="mt-6 mb-3" block color="primary" large>Login</v-btn>
 
                 <!-- <v-btn color="warning" @click="resetValidation"
                   >Reset Validation</v-btn
-                > -->
+                >-->
               </v-form>
             </v-card>
           </v-col>
@@ -73,4 +66,9 @@ export default {
   background-image: url("../assets/background.jpg");
   background-size: cover;
 }
+
+.background-color-asql {
+  background-color: #00000099;
+}
+
 </style>
